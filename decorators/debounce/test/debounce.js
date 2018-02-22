a77bb02364e3f96e579727f99de098642da70d94
Thorsten Lorenz <thlorenz@gmx.de>
2018-02-21 17:33:51 -0700

    debounce: adding test for immediate option

--- decorators/debounce/test/debounce.js
+++ decorators/debounce/test/debounce.js
@@ -11,6 +11,7 @@ class WorldClass {
     this.debouncedDefaultCount = 0
     this.debounced50Count = 0
     this.undebouncedCount = 0
+    this.debouncedImmediateCount = 0
   }
 
   static $$ontypedDebouncedDefault() {
@@ -23,6 +24,12 @@ class WorldClass {
      return [ debounce({ wait: 50 }) ]
   } ontypedDebounced50(e) {
     this.debounced50Count++
+  }
+
+  static $$ontypedDebouncedImmediate() {
+     return [ debounce({ immediate: true }) ]
+  } ontypedDebouncedImmediate() {
+    this.debouncedImmediateCount++
   }
 
   ontyped() {
@@ -53,6 +60,32 @@ test('\ndebounce: calling functions 10 times', function(t) {
     t.equal(worldClass.debouncedDefaultCount, 1, 'default debounced executes 1 time within 100 ms')
     t.equal(worldClass.debounced50Count, 2, '50 ms debounced executes 2 time within 100 ms')
     t.equal(worldClass.undebouncedCount, 10, 'undebounced executes 10 times within 100 ms')
+    t.end()
+  }
+})
+
+test('\ndebounce: immediate', function(t) {
+  const worldClass = new WorldClass()
+
+  worldClass.ontypedDebouncedDefault()
+  worldClass.ontypedDebouncedDefault()
+  worldClass.ontypedDebouncedImmediate()
+  worldClass.ontypedDebouncedImmediate()
+
+  t.equal(worldClass.debouncedDefaultCount, 0, 'default debounced executes 0 times immediately')
+  t.equal(worldClass.debouncedImmediateCount, 1, 'immediate debounced executes 1 time immediately')
+
+  setTimeout(checkAfterTimeout, 105)
+
+  function checkAfterTimeout() {
+    t.equal(worldClass.debouncedDefaultCount, 1, 'default debounced executes 1 time after 100ms')
+    t.equal(worldClass.debouncedImmediateCount, 1, 'immediate debounced executes 1 time after 100ms')
+
+    worldClass.ontypedDebouncedDefault()
+    worldClass.ontypedDebouncedImmediate()
+
+    t.equal(worldClass.debouncedDefaultCount, 1, 'default debounced does not execute 2nd time when invoked after 100ms')
+    t.equal(worldClass.debouncedImmediateCount, 2, 'immediate debounced executes 2nd time when invoked after 100ms')
     t.end()
   }
 })
