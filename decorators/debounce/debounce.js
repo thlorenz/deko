3c66f158c9fbcdbfbc608cdfd41c4a41585ed971
Thorsten Lorenz <thlorenz@gmx.de>
2018-02-21 17:42:12 -0700

    debounce: adding API docs

--- decorators/debounce/debounce.js
+++ decorators/debounce/debounce.js
@@ -16,6 +16,17 @@ const debounceDecorator = {
   , decorate: debounceDecorate
 }
 
+/**
+ * Creates a debounce decorator to be used with deko
+ *
+ * @name debounce
+ *
+ * @param {Object} $0 config
+ * @param {Number} $0.wait number of milliseconds for which to postpone executions
+ * @param {Boolean} $0.immediate if `true` causes debounce to trigger the function on the
+ * leading edge instead of the trailing edge of the wait interval
+ * @returns {Object} debounceDecorator to be used with deko
+ */
 function configureDebounce(config = {}) {
   return Object.assign(
       {}
